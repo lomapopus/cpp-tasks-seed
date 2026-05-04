239a6b908cab6a97e6cb8b668d7373a7588245ed
Artemka <memchikhuev@mail.ru>
2026-05-04 15:54:28 +0300

    ready

--- 01.InplaceSorting/sort_demo.cpp
+++ 01.InplaceSorting/sort_demo.cpp
@@ -22,9 +22,20 @@ void shuffle_data(std::vector<CollInt>& data)
     std::shuffle(data.begin(), data.end(), std::mt19937{std::random_device{}()});
 }
 
+bool check_sorted(const std::vector<CollInt>& data, const char* algo, size_t n)
+{
+    if (std::is_sorted(data.begin(), data.end()))
+    {
+        return true;
+    }
+
+    std::cerr << algo << " failed to sort array with N = " << n << "\n";
+    return false;
+}
+
 int main()
 {
-    std::vector<size_t> sizes = {100, 500, 1000, 2000};
+    std::vector<size_t> sizes = {0, 1, 2, 10, 100, 500, 1000, 2000};
 
     std::cout << "N\tAlgo\t\tComps\t\tSwaps\t\tMoves\n";
     std::cout << "----------------------------------------------------------------\n";
@@ -37,19 +48,40 @@ int main()
         shuffle_data(number_data);
         CollInt::reset_stats();
         std::sort(number_data.begin(), number_data.end());
-        std::cout << n << "\tstd::sort\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
+        const auto std_comps = CollInt::comps;
+        const auto std_swaps = CollInt::swaps;
+        const auto std_moves = CollInt::moves;
+        if (!check_sorted(number_data, "std::sort", n))
+        {
+            return 1;
+        }
+        std::cout << n << "\tstd::sort\t" << std_comps << "\t\t" << std_swaps << "\t\t" << std_moves << "\n";
 
         // --- Bubble Sort ---
         shuffle_data(number_data);
         CollInt::reset_stats();
         bubble_sort(number_data.begin(), number_data.end());
-        std::cout << n << "\tBubble\t\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
+        const auto bubble_comps = CollInt::comps;
+        const auto bubble_swaps = CollInt::swaps;
+        const auto bubble_moves = CollInt::moves;
+        if (!check_sorted(number_data, "Bubble", n))
+        {
+            return 1;
+        }
+        std::cout << n << "\tBubble\t\t" << bubble_comps << "\t\t" << bubble_swaps << "\t\t" << bubble_moves << "\n";
 
-        // --- Insertion Sort ---
+        // --- Quick Sort ---
         shuffle_data(number_data);
         CollInt::reset_stats();
         quick_sort(number_data.begin(), number_data.end());
-        std::cout << n << "\tInsertion\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
+        const auto quick_comps = CollInt::comps;
+        const auto quick_swaps = CollInt::swaps;
+        const auto quick_moves = CollInt::moves;
+        if (!check_sorted(number_data, "Quick", n))
+        {
+            return 1;
+        }
+        std::cout << n << "\tQuick\t\t" << quick_comps << "\t\t" << quick_swaps << "\t\t" << quick_moves << "\n";
 
         std::cout << "--------------------------------------------------------------\n";
     }
--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,47 @@
 #include <gtest/gtest.h>
 
-// А вот тут collvalue.h не надо, берите просто инты
+#include <algorithm>
+#include <vector>
+
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
-
-TEST(FooSuite, FooTest)
+namespace
 {
-    EXPECT_EQ(0, 0);
+
+using SortFunction = void (*)(std::vector<int>::iterator, std::vector<int>::iterator);
+
+void check_sort(SortFunction sort)
+{
+    std::vector<std::vector<int>> cases = {
+        {},
+        {1},
+        {2, 1},
+        {1, 2, 3, 4, 5},
+        {5, 4, 3, 2, 1},
+        {4, 2, 5, 2, 1, 4, 3},
+        {0, -3, 7, -3, 2, 0, 9, 1},
+    };
+
+    for (auto data : cases)
+    {
+        auto expected = data;
+        std::sort(expected.begin(), expected.end());
+
+        sort(data.begin(), data.end());
+
+        EXPECT_EQ(expected, data);
+        EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+    }
+}
+
+} // namespace
+
+TEST(SortingSuite, BubbleSortWorks)
+{
+    check_sort(bubble_sort<std::vector<int>::iterator>);
+}
+
+TEST(SortingSuite, QuickSortWorks)
+{
+    check_sort(quick_sort<std::vector<int>::iterator>);
 }
